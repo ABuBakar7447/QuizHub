98f9d8249e0001236755ae8aa898bb4ecb839d79
ABuBakar7447 <lone7447wolf@gmail.com>
2022-10-11 00:25:52 +0600

    404 route organized

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './layouts/Main';
+import Home from './components/Home/Home';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path:'/',
+      element:<Main></Main>,
+      children: [
+        {
+          path: '/',
+          loader: () =>fetch('fakedata.json'),
+          element:<Home></Home>
+        }
+      ]
+      
+    },
+    {
+      path:'*',
+      element: <div>This is not correct page You are seaching for please go back</div>
+    }
+  ])
   return (
     <div className="App">
-      
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
